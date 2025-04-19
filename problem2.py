c008b3fce98ffcdc271847ca9fb2e98146200793
Tarun2840 <122767842+Tarun2840@users.noreply.github.com>
2025-04-19 22:39:33 +0530

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -1,15 +1,26 @@
-def generate_odd_series(a):
-    if a <= 0:
+def generate_odd_numbers(n):
+    if n <= 0:
         return ""
     odd_numbers = []
     count = 0
     num = 1
-    while count < a:
+    while count < n:
         odd_numbers.append(str(num))
         num += 2
         count += 1
     return ",".join(odd_numbers)
 
+
+def main():
+    try:
+        n = int(input("Enter the number of odd numbers to generate: "))
+        if n <= 0:
+            print("Please enter a positive integer.")
+        else:
+            series = generate_odd_numbers(n)
+            print("Odd Number Series:", series)
+    except ValueError:
+        print("Invalid input. Please enter an integer.")
+
 if __name__ == "__main__":
-    a = int(input("Enter the number of odd numbers to generate: "))
-    print("Odd Number Series:", generate_odd_series(a))
+    main()
